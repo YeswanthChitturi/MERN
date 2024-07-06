9ccaf58fe6da07a2117b91becee4c935a8538f05
YeswanthChitturi <yeswanthkanna6281@gmail.com>
2024-07-06 15:37:56 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import Navbar from './components/navbar';
+import Register from './components/Register';
+import Login from './components/Login';
+import About from './components/About';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<div>
+ <Router>
+ <Navbar/>
+ <Routes>
+ <Route path="/login" element={<Login/>}/>
+ <Route path="/about" element={<About/>}/>
+ <Route path="/register" element={<Register/>}/>
+ </Routes>
+ </Router>
+
+</div>
+);
